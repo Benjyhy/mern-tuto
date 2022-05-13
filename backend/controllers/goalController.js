63b8d9af0c864b671a4db145d487ec4eeaab62e8
RAMET Benjamin <benjy.ramet@yahoo.fr>
2022-05-13 16:06:12 +0200

    initial rest api for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,33 +1,54 @@
 import asyncHandler from "express-async-handler";
+import Goal from "../models/goalModel.js";
 
 // @desc GET goals
 // @route GET /api/goals
 // @access Private
 export const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Get Goals" });
+    const goals = await Goal.find();
+    res.status(200).json(goals);
 });
 
 // @desc POST goal
 // @route POST /api/goals
 // @access Private
 export const setGoal = asyncHandler(async (req, res) => {
-    if (!req.body.try) {
+    if (!req.body.text) {
         res.status(400);
-        throw new Error("Please add a try field.");
+        throw new Error("Please add a text field.");
     }
-    res.status(200).json({ message: "Set Goals" });
+    const goal = await Goal.create({
+        text: req.body.text,
+    });
+    res.status(200).json(goal);
 });
 
 // @desc PUT goals
 // @route PUT /api/goals/:id
 // @access Private
 export const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `update goal ${req.params.id}` });
+    const goal = await Goal.findById(req.params.id);
+    if (!goal) {
+        res.status(400);
+        throw new Error("Goal not found");
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    });
+    res.status(200).json(updatedGoal);
 });
 
 // @desc DELETE goals
 // @route DELETE /api/goals/:id
 // @access Private
 export const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `delete goal ${req.params.id}` });
+    const goal = await Goal.findById(req.params.id);
+    if (!goal) {
+        res.status(400);
+        throw new Error("Goal not found");
+    }
+
+    goal.remove(req.params.id);
+    res.status(200).json({ id: req.params.id });
 });
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from "express";
+import colors from "colors";
 import dotenv from "dotenv";
 import goalRoutes from "./routes/goalRoutes.js";
 import { errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
+dotenv.config();
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
